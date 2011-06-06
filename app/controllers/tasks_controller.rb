88dada7ef590b5d431111fd9c034d451653e3b3e
José Corcuera Z <jose@bell-n-whistle.com>
2011-06-06 17:58:03 -0500

    Using faye on controller and views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,7 +6,12 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    @task.update_attributes(params[:task])
+    if @task.update_attributes(params[:task])
+      message = {:channel => project_path(@task.project), :data => { :task_id => @task.id, :task_state => @task.state}}
+      uri = URI.parse("http://localhost:9292/faye")
+      Net::HTTP.post_form(uri, :message => message.to_json)
+    end
+    render :nothing => true
   end
 
   def destroy
